edd639583e29ffe00e10d7c7ff581dae02e5187e
Miles Eng <miles.eng@duke.edu>
2023-10-08 12:39:59 -0400

    updated production build

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :content`))
 
 morgan.token('content', function(request, response) {
