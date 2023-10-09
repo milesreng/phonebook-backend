2417fcc62c30b4e16f0f4bb142517974b4b86f86
Miles Eng <miles.eng@duke.edu>
2023-10-08 20:04:45 -0400

    uibuild

--- index.js
+++ index.js
@@ -1,70 +1,63 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+
 const app = express()
+const Contact = require('./models/contact')
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :content`))
+app.use(errorHandler)
 
 morgan.token('content', function(request, response) {
     return JSON.stringify(request.body)
 })
 
-let data = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// app.get('/info', (request, response) => {
+//     const count = data.length
+//     const date = new Date()
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello world</h1>')
-})
-
-app.get('/info', (request, response) => {
-    const count = data.length
-    const date = new Date()
-
-    response.send(`<p>Phone book has information for ${count} people</p>
-                        <p>${date}</p>`)
-})
+//     response.send(`<p>Phone book has information for ${count} people</p>
+//                         <p>${date}</p>`)
+// })
 
 app.get('/api/persons', (request, response) => {
-    response.json(data)
+    Contact.find({}).then(contacts => {
+        response.json(contacts)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const pid = Number(request.params.id)
-    const person = data.find(p => p.id === pid)
+app.get('/api/persons/:id', (request, response, next) => {
+    Contact.findById(request.params.id).then(contact => {
+        if (contact) {
+            response.json(contact)
+        } else {
+            response.status(404).end()
+        }
+    }).catch(error => next(error))
+    
+    // const pid = Number(request.params.id)
+    // const person = data.find(p => p.id === pid)
 
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(400).end()
-    }
+    // if (person) {
+    //     response.json(person)
+    // } else {
+    //     response.status(400).end()
+    // }
 })
-
-const generateID = () => {
-    return Math.floor(Math.random() * 10000)
-}
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -75,31 +68,39 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const contact = {
+    const contact = new Contact({
         name: body.name,
-        number: body.number,
-        id: generateID(),
-    }
+        number: body.number
+    })
 
-    if (data.find(person => person.name == contact.name)) {
-        return response.status(400).json({
-            error: `${contact.name} already exists in phonebook`
-        })
-    }
+    
 
-    data = data.concat(contact)
-    response.json(contact)
+    // if (data.find(person => person.name == contact.name)) {
+    //     return response.status(400).json({
+    //         error: `${contact.name} already exists in phonebook`
+    //     })
+    // }
+
+    contact.save().then(savedContact => {
+        response.json(savedContact)
+    })
+})
+
+app.put('/api/persons/:id', (request, response) => {
+    const body = request.body
+
+    Contact.findByIdAndUpdate(request.params.id, body).then(contact => {
+        response.json(contact)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const pid = Number(request.params.id)
-
-    data = data.filter(person => person.id !== pid)
-
-    response.status(204).end()
+    Contact.findByIdAndRemove(request.params.id).then(result => {
+        response.status(204).end()
+    }).catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
---
+++ mongo.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+} 
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.czlxalm.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Contact = mongoose.model('Contact', contactSchema)
+
+if (process.argv.length < 5) {
+    console.log('phonebook:')
+    Contact.find({}).then(result => {
+        result.forEach(contact => {
+            console.log(`${contact.name} ${contact.number}`)
+        })
+        mongoose.connection.close()
+    })
+} else {
+    const newName = process.argv[3]
+    const newPhone = process.argv[4]
+
+    const contact = new Contact({
+        name: newName,
+        number: newPhone,
+    })
+
+    contact.save().then(result => {
+        console.log(`added ${newName} number ${newPhone} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+
+
+// const contact = new Note({
+//   content: 'HTML is Easy',
+//   important: true,
+// })
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
